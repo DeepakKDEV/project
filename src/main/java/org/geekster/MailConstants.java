25d5b8e4a5fd4b6eb353a0c6791ac6cac65443d8
gitpractce <deepak76311@gmail.com>
2023-09-05 01:29:38 +0530

    project

---
+++ src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,7 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "ram8888@gmail.com"; //random mail
+    public static final String APP_PASSWORD = "App password required" ;
+}
